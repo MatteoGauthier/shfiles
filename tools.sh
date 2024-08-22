11560dec9e5cb3734b159c909f1d40bb2997fb03
Mattèo Gauthier <matteo.gauthier@gmail.com>
2024-08-22 15:12:30 +0200

    Not working function frg, need to be revised. rg + tui to search and preview

--- tools.sh
+++ tools.sh
@@ -52,3 +52,17 @@ qrserve () {
     && python -m http.server $port -b $ip -d $dir
 }
 
+# Not working !! @xxx @todo
+function frg {
+    result=$(rg --ignore-case --color=always --line-number --no-heading "$@" |
+        fzf --ansi \
+            --color 'hl:-1:underline,hl+:-1:underline:reverse' \
+            --delimiter ':' \
+            --preview "bat --color=always {1} --theme='Solarized (light)' --highlight-line {2}" \
+            --preview-window 'up,60%,border-bottom,+{2}+3/3,~3')
+    file=${result%%:*}
+    linenumber=$(echo "${result}" | cut -d: -f2)
+    if [[ -n "$file" ]]; then
+        $EDITOR +"${linenumber}" "$file"
+    fi
+}
