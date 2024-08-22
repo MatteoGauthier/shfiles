043b2fa8cf09ed59147b823287b25e9746ab5d47
Mattèo Gauthier <matteo.gauthier@gmail.com>
2024-08-22 15:10:45 +0200

    Add HISTTIMEFORMAT env var

--- config.sh
+++ config.sh
@@ -1,7 +1,8 @@
-export LANG=en_US.UTF-8
+export LANG="en_US.UTF-8"
 
 # export FZF_DEFAULT_COMMAND="rg --files --hidden --follow --no-ignore -g '!{node_modules,.git,Library}'"
 export FZF_DEFAULT_COMMAND="rg --files --hidden --follow --no-ignore -g '!{node_modules,.git,Library} --max-depth 1'"
 
 export HISTSIZE=50000
 export SAVEHIST=50000
+export HISTTIMEFORMAT="%d/%m/%y %T "
