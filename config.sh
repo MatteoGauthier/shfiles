eaa071b34d7e28d7473d6b64d519d2d6d8794ee5
Mattèo Gauthier <matteo.gauthier@gmail.com>
2024-08-28 09:44:22 +0200

    [CONFIG] Add new fzf option

--- config.sh
+++ config.sh
@@ -2,6 +2,7 @@ export LANG="en_US.UTF-8"
 
 # export FZF_DEFAULT_COMMAND="rg --files --hidden --follow --no-ignore -g '!{node_modules,.git,Library}'"
 export FZF_DEFAULT_COMMAND="rg --files --hidden --follow --no-ignore -g '!{node_modules,.git,Library} --max-depth 1'"
+export FZF_DEFAULT_OPTS='--multi --no-height --extended'
 
 export HISTSIZE=50000
 export SAVEHIST=50000
