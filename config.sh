ddf5f90433501d7267a6779b7a105a152261c818
Mattèo Gauthier <matteo.gauthier@gmail.com>
2024-10-23 16:09:25 +0200

    [CONFIG] FZF opts

--- config.sh
+++ config.sh
@@ -1,7 +1,8 @@
 export LANG="en_US.UTF-8"
 
-# export FZF_DEFAULT_COMMAND="rg --files --hidden --follow --no-ignore -g '!{node_modules,.git,Library}'"
-export FZF_DEFAULT_COMMAND="rg --files --hidden --follow --no-ignore -g '!{node_modules,.git,Library} --max-depth 1'"
+# export FZF_DEFAULT_COMMAND="rg --files --hidden --follow --no-ignore -g '!{node_modules/,.git/,Library}'"
+# export FZF_DEFAULT_COMMAND="rg --files --hidden --follow"
+export FZF_DEFAULT_COMMAND="rg --files --hidden --follow --smart-case -g '!node_modules' -g '!.git' -g '!Library'"
 export FZF_DEFAULT_OPTS='--multi --no-height --extended'
 
 export HISTSIZE=50000
